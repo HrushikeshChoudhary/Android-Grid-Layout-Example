e85481d85a4747064aee2d4fb7fa5b9c6e54b17a
Hrushikesh Choudhary <hrushikeshchoudhary@gmail.com>
2017-11-14 22:01:32 +0530

    Added the source code

---
+++ java/com/example/hrushikesh/gridlayout/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.hrushikesh.gridlayout;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void buttonTapped(View view) {
+
+        int id = view.getId();
+
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = view.getResources().getIdentifier(ourId, "raw", "com.example.hrushikesh.gridlayout");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
+
+        Log.i("Button Tapped",ourId);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
